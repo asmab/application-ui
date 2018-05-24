7ba7f056cc06efd2bca8ed333c4b86824974df2d
asma <asma.boussabat.asma@gmail.com>
2018-05-24 09:58:26 +0200

    interface+style

--- src/components/TriangleForm.js
+++ src/components/TriangleForm.js
@@ -1,86 +1,110 @@
 import React from 'react'
 
 const TriangleForm = Component => 
-  class HigherOrderComponent extends React.Component {
+  class TriangleComponent extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { form: props.initialState }
+    this.state = { form: props.initialState, result: "" }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.reset = this.reset.bind(this)
   }
   handleSubmit(e) {
      e.preventDefault()
-     this.state.result = this.triangleTypeSolver(this.state.form)
+     const result = this.triangleTypeSolver(this.state.form)
+     this.setState({ result })
   }
 
   handleChange(e) {
     const target = e.target
     const name = target.name
-    const value = target.type === 'checkbox' 
-      ? target.checked 
-      : target.value
-      
+    const value = target.value      
     this.setState(state => ({form: {...state.form, ...{[name] : value}}})) 
+  }
+
+  reset(e) {
+    e.persist()
+    ts.ui.Dialog.confirm('Are you sure?', {
+      onaccept: function() { 
+        
+        ts.ui.Notification.success('Values cleared')
+      },
+      oncancel: function() {
+        ts.ui.Notification.success('Finish the test')
+      }
+    })
   }
 
   triangleTypeSolver(vals) {
     if ((vals.sideA == vals.sideB) || (vals.sideB == vals.sideC) || (vals.sideA == vals.sideC)) {
-      if ((vals.sideA == vals.sideB) && (vals.sideA == vals.sideC) && (vals.sideB == vals.sideC))
-        return "equilateral";
-      return "isosceles";
+      if (vals.sideA == vals.sideB && vals.sideB == vals.sideC)
+        return "equilateral"
+      return "isosceles"
     }
-    return "scalene";
+    return "scalene"
   }
 
   render() {
     const { initialState, ...rest } = this.props
-    const { form } = this.state
-    return React.createElement(Component, {
-      state: form,
+    const { form, result  } = this.state
+    return React.createElement(Form, {
+      state: { ...form, result },
       handleSubmit: this.handleSubmit,
-      handleChange: this.handleChange
+      handleChange: this.handleChange,
+      reset: this.reset
     })
   }
 }
 
-const Form = ({state, handleSubmit, handleChange}) => 
-   (<div>
-   <form data-ts="Form" onSubmit={handleSubmit}>
-    <fieldset>
-      <label>
-        <span> Side A:</span>
-          <input
-            type="number"
-            name="sideA"
-            value={state.sideA}
-            onChange={handleChange}
-          />
-      </label>
-      <label>
-        <span> Side B:</span>
-          <input
-            type="number"
-            name="sideB"
-            value={state.sideB}
-            onChange={handleChange}
-          />
-      </label>
-      <label>
-        <span> Side C:</span>
-          <input
-            type="number"
-            name="sideC"
-            value={state.sideC}
-            onChange={handleChange}
-          />
-      </label>
-       <button data-ts="Button" className="ts-primary" type="submit">
-           <span>Submit</span>
-        </button>
-    </fieldset>
-</form>
-<h1> result : {state.result}</h1>
-</div>)
+const Form = ({state, handleSubmit, handleChange, reset}) => 
+   (<div className="container">
+        <div className="row">
+            <div className="col">
+                <form data-ts="Form" onSubmit={handleSubmit}>
+                  <fieldset>
+                    <label>
+                      <span> Side A:</span>
+                        <input
+                          type="number"
+                          name="sideA"
+                          value={state.sideA}
+                          onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                      <span> Side B:</span>
+                        <input
+                          type="number"
+                          name="sideB"
+                          value={state.sideB}
+                          onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                      <span> Side C:</span>
+                        <input
+                          type="number"
+                          name="sideC"
+                          value={state.sideC}
+                          onChange={handleChange}
+                        />
+                    </label>
+                    <button data-ts="Button" className="ts-primary submit-button" type="button" onClick={reset}>
+                        <span>Clear</span>
+                    </button>
+                    <button data-ts="Button" className="ts-primary submit-button" type="submit">
+                        <span>Submit</span>
+                    </button>
+                  </fieldset>
+              </form>
+            </div>
+            <div className="col">
+              Triangle type :
+              <p className="result-class"> { state.result } </p>
+            </div>
+        </div>
+    </div>
+)
 
 export default TriangleForm(Form)
--- src/components/landing/Section.js
+++ src/components/landing/Section.js
@@ -1,32 +1,22 @@
 import React from 'react'
 import TriangleForm from '../TriangleForm'
-const Section = () => {
 
-    //  ReactDOM.render(
 
-    //     ts.ui.ready(function() {
-    //         var component = ts.ui.get(element)
-    //         component.dosomething();
-    //     })
-    // )
-
+const Section = () => {   
 
     return (
+        <section>
+            <span>Insert the lengths of the triangle's three sides A,B and C 
+                and press Resolve button to check the triangle type
+            </span>
 
-        <section className="jumbotron jumbotron-fluid white-bg">
-
-            <p> test tradeshift ui components </p>
-
-            <div className="triangleForm">
-                
-                <TriangleForm 
-                    initialState={{
-                        sideA: '',
-                        sideB: '',
-                        sideC: ''
-                    }}
-                />
-            </div>
+            <TriangleForm 
+            initialState={{
+                sideA: '',
+                sideB: '',
+                sideC: ''
+            }}
+            />
         </section>
     )
 }
